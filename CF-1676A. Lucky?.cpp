4c5eec169eac226e31711a45c28bbc626fd04743
ArpitaTrisha12 <arpitabhadratrisha@gmail.com>
2025-12-05 01:13:20 +0600

    Create CF-1676A. Lucky?.cpp

---
+++ CF-1676A. Lucky?.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    string m;
+cin>>m;
+if((m[0]+m[1]+m[2]) ==(m[3]+m[4]+m[5])){
+    cout<<"YES"<<endl;
+
+}
+else{
+    cout<<"NO"<<endl;
+}
+
+}
+
+return 0;
+
+}
