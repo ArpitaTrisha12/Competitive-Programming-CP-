21f9d8d44dba9822430d21f80799567ec3524bd6
ArpitaTrisha12 <arpitabhadratrisha@gmail.com>
2025-12-05 15:08:35 +0600

    Create string palindrome or not.cpp

---
+++ string palindrome or not.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+
+    string b = a; 
+    reverse(b.begin(), b.end());
+
+    if (a == b) 
+        cout << "Palindrome\n";
+    else 
+        cout << "Not Palindrome\n";
+
+    return 0;
+}
