69a511b15538ffcdb13b1bc978b69574c1f8faf3
ArpitaTrisha12 <arpitabhadratrisha@gmail.com>
2025-12-09 11:42:57 +0600

    Update DFS using adjacency list for undirected graph.cpp

--- DFS using adjacency list for undirected graph.cpp
+++ DFS using adjacency list for undirected graph.cpp
@@ -1,31 +1,21 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int vis[1000];
-vector<int> adj[10000];
-void dfs(int node) {
-    vis[node] = 1;
-    cout << node << " -> ";
-
-    for (int child : adj[node]) {
-        if (!vis[child]) {
-            dfs(child);
-        }
-    }
-}
-int main() {
-    int n, m;
-    cin >> n >> m;
-    while (m--) {
-        int x, y;
-        cin >> x >> y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-    for (int i = 1; i <= n; i++) {
-        if (!vis[i]) {
-            dfs(i);
-        }
-    }
-    cout << endl;
-    return 0;
-}
+vector<int> v[10000];
+void dfs(int node){
+vis[node]=1;
+cout<<node<<"->";
+for(int child:v[node]){
+if(!vis[child])dfs(child);
+}}
+int main(){
+int n,m,x,y;
+cin>>n>>m;
+while(m--){
+cin>>x>>y;
+v[x].push_back(y);
+v[y].push_back(x);}
+for(int i=0;i<n;i++)//0 indexed {
+if(!vis[i]){
+dfs(i);
+cout<<endl;}}
